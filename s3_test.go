82e012c6b4ec193d7fa41564e672d4b06747a3e4
Dmitry Kolesnikov <dmkolesnikov@gmail.com>
2020-01-04 16:26:37 +0200

    add match test for s3

--- s3_test.go
+++ s3_test.go
@@ -27,6 +27,38 @@ func TestS3Put(t *testing.T) {
 	it.Ok(t).If(apiS3().Put(entity())).Should().Equal(nil)
 }
 
+func TestS3Remove(t *testing.T) {
+	it.Ok(t).If(apiS3().Remove(entity())).Should().Equal(nil)
+}
+
+func TestS3Update(t *testing.T) {
+	val := person{IRI: dynamo.IRI{"dead", "beef"}}
+	err := apiS3().Update(&val)
+
+	it.Ok(t).
+		If(err).Should().Equal(nil).
+		If(val).Should().Equal(entity())
+}
+
+func TestS3Match(t *testing.T) {
+	cnt := 0
+	seq := apiS3().Match(dynamo.IRI{Prefix: "dead"})
+
+	for seq.Tail() {
+		cnt++
+		val := person{}
+		err := seq.Head(&val)
+
+		it.Ok(t).
+			If(err).Should().Equal(nil).
+			If(val).Should().Equal(entity())
+	}
+
+	it.Ok(t).
+		If(seq.Error()).Should().Equal(nil).
+		If(cnt).Should().Equal(2)
+}
+
 //-----------------------------------------------------------------------------
 //
 // Mock S3
@@ -64,4 +96,21 @@ func (mockS3) PutObject(input *s3.PutObjectInput) (*s3.PutObjectOutput, error) {
 	}
 
 	return &s3.PutObjectOutput{}, nil
+}
+
+func (mockS3) DeleteObject(input *s3.DeleteObjectInput) (*s3.DeleteObjectOutput, error) {
+	if aws.StringValue(input.Key) != "dead/beef" {
+		return nil, errors.New("Unexpected entity. ")
+	}
+
+	return &s3.DeleteObjectOutput{}, nil
+}
+
+func (mockS3) ListObjectsV2(*s3.ListObjectsV2Input) (*s3.ListObjectsV2Output, error) {
+	return &s3.ListObjectsV2Output{
+		Contents: []*s3.Object{
+			{Key: aws.String("dead/beef")},
+			{Key: aws.String("dead/beef")},
+		},
+	}, nil
 }
